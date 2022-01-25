b2ec2b66af03d04ccb994e7169a2368ec7d60d89
Sahil Saini <sahilsaini137@gmail.com>
2022-01-26 00:32:24 +0530

    add details page

---
+++ details.js
@@ -0,0 +1,45 @@
+const urlParams = new URLSearchParams(window.location.search);
+const movieId = urlParams.get("id");
+const APIkey = "e6df134c";
+
+const container = document.getElementById("main-container");
+
+window.onload = fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=${APIkey}`)
+  .then((response) => response.json())
+  .then((data) => displayDetails(data));
+
+function displayDetails(data) {
+  console.log(data);
+  let content = `<div class="left-container">
+      <div class="poster-container">
+          <img src="${data.Poster}" class="movie-poster">
+      </div>
+  </div>
+  <div div class="right-container">
+      <div>
+          <h1 class="details-heading">${data.Title}</h1>
+      </div>
+      <div class = "genre">
+      Genre: ${data.Genre}
+      </div>
+      <div class="actors">
+      Actors: ${data.Actors} &nbsp;
+      Director: ${data.Director}
+      </div>
+      <div class="plot">
+      <p>Plot: ${data.Plot}</p>
+      </div>
+      <div class="Release">
+      Release Date: ${data.Released}
+      </div>
+      <div>
+      Language: ${data.Language}
+      </div>
+      <div>
+      IMDB rating: ${data.imdbRating}
+      </div>
+
+      </div>
+      `;
+  container.innerHTML = content;
+}
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let searchTextElement = document.getElementById("search-movie-input");
 const searchResultContainer = document.getElementById("movie-result-container");
 // Event Listeners
 searchTextElement.addEventListener("input", searchForMovie);
+searchResultContainer.addEventListener("click", getMoreDetails);
 
 // functions
 const APIkey = "e6df134c";
@@ -32,4 +33,12 @@ function searchForMovie() {
       }
       searchResultContainer.innerHTML = list;
     });
+}
+
+function getMoreDetails(event) {
+  if (event.target.classList.contains("details-button")) {
+    let movie = event.target.parentElement.parentElement;
+    const id = movie.id;
+    window.open("./details.html" + "?id=" + id);
+  }
 }
